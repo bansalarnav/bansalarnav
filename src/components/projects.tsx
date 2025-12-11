62f7cc30ea7ecca2e48c7d9e3a256b559f2926ae
Arnav Bansal <arnavbansal101@gmail.com>
2025-12-10 21:34:28 -0600

    add projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,31 +1,80 @@
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+
 export const Projects = () => {
-  const projects = [];
+  const projects = [
+    {
+      name: "Cyclr",
+      year: 2025,
+      description:
+        "Bike sharing platform for college campuses. Complete with app and smart locks.",
+      builtWith: "React Native, Elysia.js, PostgreSQL + Drizzle, PlatformIO",
+      color: "#4886F7",
+      link: "https://github.com/bansalarnav/cyclr",
+    },
+    {
+      name: "Canvas Assignment Exporter",
+      year: 2025,
+      description: "Export assignments from Canvas to a Notion Database",
+      builtWith: "SvelteKit, Notion API",
+      color: "#F5A267",
+      link: "https://canvas.arnav.fish",
+    },
+    {
+      name: "Smart Jab",
+      year: 2022,
+      description: "App to find COVID-19 vaccination slots in India",
+      builtWith: "Flutter, React, Express",
+      color: "#E69EFE",
+      link: "https://github.com/chiragbhansali/smart-jab-mobile",
+    },
+  ];
+
   return (
     <div className="text-[#474747] text-[15px] sm:text-[17px] mt-[4px]">
       I like to keep working on personal projects whenever I can. Here are a few
       of them:
       <div>
-        <ProjectCard color={"#f00"} />
-        <ProjectCard color={"#0f0"} />
-        <ProjectCard color={"#00f"} />
+        {projects.map((project) => (
+          <ProjectCard key={project.name} {...project} />
+        ))}
       </div>
     </div>
   );
 };
 
-const ProjectCard = ({ color }: { color: string }) => {
+const ProjectCard = ({
+  color,
+  name,
+  year,
+  description,
+  builtWith,
+  link,
+}: {
+  color: string;
+  name: string;
+  year: number;
+  description: string;
+  builtWith: string;
+  link: string;
+}) => {
   return (
     <div
       className={`border-l-[4px] sm:border-l-[6px] my-[12px] sm:my-[16px] py-[2px] px-[8px] sm:px-[12px] text-[14px] sm:text-[16px]`}
       style={{ borderColor: color }}
     >
       <div className="flex justify-between items-center flex-wrap gap-[4px]">
-        <h4 className="text-[16px] sm:text-[18px]">Cyclr</h4>
-        <p className="text-[14px] sm:text-[16px]">2025</p>
+        <Link href={link} target="_blank" rel="noopener noreferrer">
+          <h4 className="text-[15px] sm:text-[17px] text-[#000] font-[quando] flex items-center cursor-pointer hover:underline">
+            {name}
+            <ExternalLink size={14} className="ml-[5px] mb-[1px]" />
+          </h4>
+        </Link>
+        <p className="text-[14px] sm:text-[16px] italic">{year}</p>
       </div>
-      <p className="text-[14px] sm:text-[16px]">A brief description</p>
-      <div className="text-[13px] sm:text-[15px]">
-        Built with: React Native, Elysia.js, Drizzle, PlatformIO
+      <p className="text-[14px] sm:text-[16px]">{description}</p>
+      <div className="text-[13px] sm:text-[15px] mt-[4px]">
+        Built with: {builtWith}
       </div>
     </div>
   );
